db5de4d59c9bd89cca8f376438c296024645d876
Miyuki Nozawa <nozawa.myk@gmail.com>
2022-04-10 22:34:05 -0400

    display notes

--- src/Notes.js
+++ src/Notes.js
@@ -1,16 +1,58 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addNote, deleteNote } from "./store";
+import { Link } from "react-router-dom";
 
-const Notes = ()=> {
-  return (
-    <div>
-      <Link to='/home'>Home</Link>
+class Notes extends Component {
+  state = {
+    addNoteText: "",
+  };
+
+  handleDelete = async (id) => {
+    await this.props.deleteNote(id);
+  };
+
+  handleChange = (event) => {
+    this.setState({ addNoteText: event.target.value });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    await this.props.addNote({ txt: this.state.addNoteText });
+    this.setState({ addNoteText: "" });
+  };
+
+  render() {
+    const { notes } = this.props;
+
+    return (
       <div>
-        TODO - Ability of User to manage notes
+        <Link to="/home">Home</Link>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              placeholder="Add Note"
+              onChange={this.handleChange}
+              value={this.state.addNoteText}
+            />
+            <button onClick={this.handleSubmit}>Add</button>
+          </form>
+          {notes.map((note) => (
+            <div key={note.id}>
+              {note.txt}
+              <button onClick={() => this.handleDelete(note.id)}>Delete</button>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default connect()(Notes);
+const mapStateToProps = (state) => ({ notes: state.notes });
+const mapDispatchToProps = (dispatch) => ({
+  addNote: (note) => dispatch(addNote(note)),
+  deleteNote: (id) => dispatch(deleteNote(id)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Notes);
